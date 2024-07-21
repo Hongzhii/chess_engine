e0b6acbcc39a3dee6aca2c0929a58971b65b6bb6
HZ <hongzhiee@gmail.com>
2024-07-21 13:08:22 +0800

    overload bitwise operations for bitboard and implement num_pieces method

--- bitboard.py
+++ bitboard.py
@@ -1,6 +1,31 @@
+from typing import Optional
+
 class BitBoard:
-    def __init__(self):
-        self.bitboard = int('0b0', 2)
+    """
+    Bitboard class data class that contains information about the location of
+    pieces on the chessboard. Also contains helper utility functions for common
+    bitboard operations.
+
+    Attributes:
+        bitboard (int): Integer storing binary representation of a bitboard
+    """
+
+    def __init__(self, bitboard: Optional['BitBoard'] = None):
+        if bitboard is not None:
+            self.bitboard = bitboard.bitboard
+        else:
+            self.bitboard = int('0b0', 2)
+
+    def add(self, bitboard: 'BitBoard') -> None:
+        """
+        Performs binary OR operation accross two bitboards and applies the
+        result to the current bitboard.
+
+        Args:
+            bitboard (BitBoard): BitBoard to be added to current bitboard
+        """
+
+        self.bitboard = self.bitboard | bitboard.bitboard
 
     def set(self, row: int, col: int) -> None:
         """
@@ -15,7 +40,7 @@ class BitBoard:
 
         # row 7 col 7 is actually first position on the bitboard
         position = 63 - position
-        assert position >= 0
+        assert position >= 0, f"Got position: {position}"
 
         mask = 1 << position
 
@@ -72,8 +97,15 @@ class BitBoard:
         result = "0" * (64-len(result)) + result
 
         # Reverse bitboard string (position 7, 7 is first position of bitboard)
+        print("="*40)
         for i in range(8):
             print(result[8*i:8*i+8])
+        print("="*40)
+
+    def get_num_pieces(self) -> int:
+        # Convert to binary string representation
+        bin_str = bin(self.bitboard)
+        return bin_str.count("1")
 
 
 if __name__ == "__main__":
@@ -87,4 +119,15 @@ if __name__ == "__main__":
     val = 1 << 63 - (8*4 + 5)
     assert bitboard.bitboard ^ val == 0
 
-    bitboard.print()
+    bitboard_copy = BitBoard(bitboard)
+    bitboard_copy.unset(4, 5)
+    bitboard_copy.set(1, 3)
+    bitboard_copy.set(3, 3)
+
+    bitboard.show()
+    bitboard_copy.show()
+
+    bitboard.add(bitboard_copy)
+    bitboard.show()
+
+    assert bitboard.get_num_pieces() == 3, f"{bitboard.get_num_pieces()}"
