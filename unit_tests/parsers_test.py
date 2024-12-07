b881159eaa28f3111759332e49546198e4387737
HongZhii <hongzhiee@gmail.com>
2024-12-07 22:24:30 +0800

    fix coordinate conversion function test

--- unit_tests/parsers_test.py
+++ unit_tests/parsers_test.py
@@ -5,19 +5,19 @@ from src import parsers
 class TestParser(unittest.TestCase):
     def test_alphanumeric(self):
         position_input = "a1"
-        expected_output = (0, 0)
-        self.assertTrue(
-            parsers.alphanumeric_to_index(position_input, expected_output)
+        expected_output = (7, 0)
+        self.assertEqual(
+            parsers.alphanumeric_to_index(position_input), expected_output
         )
 
         position_input = "h8"
-        expected_output = (7, 7)
-        self.assertTrue(
-            parsers.alphanumeric_to_index(position_input, expected_output)
+        expected_output = (0, 7)
+        self.assertEqual(
+            parsers.alphanumeric_to_index(position_input), expected_output
         )
 
         position_input = "e4"
-        expected_output = (4, 3)
-        self.assertTrue(
-            parsers.alphanumeric_to_index(position_input, expected_output)
+        expected_output = (4, 4)
+        self.assertEqual(
+            parsers.alphanumeric_to_index(position_input), expected_output
         )
