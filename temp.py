627c0b1403a62988b60adac0b1ba94dbf38dfca0
HongZhii <hongzhiee@gmail.com>
2024-12-14 12:27:16 +0800

    delete temp.py

--- temp.py
+++
@@ -1,8 +0,0 @@
-from src.chess_board.bitboard import BitBoard
-from src.chess_board.board import Board
-from resources import FENs
-
-board = Board(fen_string=FENs.ILLEGAL_CASTLING_BLACK_IN_CHECK)
-# board = Board()
-
-board.get_legal_moves()
